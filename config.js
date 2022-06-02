6fb1fb15566e5b77274fdec6270d22d02ae284d0
gribanov <gribanov@protei.ru>
2022-06-02 13:11:29 +0300

    Corrected Heroku vars

--- config.js
+++ config.js
@@ -1,9 +1,9 @@
 const config = {
     db: {
-        host: "process.env.DB_HOST",
-        user: "process.env.DB_USER",
-        password: "process.env.DB_PWD",
-        database: "process.env.DB_SCHEMA"
+        host: process.env.DB_HOST,
+        user: process.env.DB_USER,
+        password: process.env.DB_PWD,
+        database: process.env.DB_SCHEMA
     }
 }
 
